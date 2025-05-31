92485d9ae45f141d9f1558cfc7268f579aa9a1de
Angry Coding <mailidanielifechukwu@gmail.com>
2025-05-31 05:51:35 +0000

    chore: fixes

--- sui-utils.ts
+++ sui-utils.ts
@@ -20,87 +20,9 @@ export const getActiveAddress = () => {
     return execSync(`${SUI_BIN} client active-address`, { encoding: "utf8" }).trim();
 };
 
-/** Returns a signer based on the active address of system's sui. */
-export const getSigner = () => {
-    const sender = getActiveAddress();
-
-    const keystore = JSON.parse(
-        readFileSync(path.join(homedir(), ".sui", "sui_config", "sui.keystore"), "utf8"),
-    );
-
-    for (const priv of keystore) {
-        const raw = fromBase64(priv);
-        if (raw[0] !== 0) {
-            continue;
-        }
-
-        const pair = Ed25519Keypair.fromSecretKey(raw.slice(1));
-        if (pair.getPublicKey().toSuiAddress() === sender) {
-            return pair;
-        }
-    }
-
-    throw new Error(`keypair not found for sender: ${sender}`);
-};
 
 /** Get the client for the specified network. */
 export const getClient = (network: Network) => {
     const url = process.env.SUI_RPC_URL || getFullnodeUrl(network);
     return new SuiClient({ url });
-};
-
-/** A helper to sign & execute a transaction. */
-export const signAndExecute = async (txb: Transaction, network: Network) => {
-    const client = getClient(network);
-    const signer = getSigner();
-
-    return client.signAndExecuteTransaction({
-        transaction: txb,
-        signer,
-        options: {
-            showEffects: true,
-            showObjectChanges: true,
-        },
-    });
-};
-
-/** Publishes a package and saves the package id to a specified json file. */
-export const publishPackage = async ({
-    packagePath,
-    network,
-    exportFileName = "contract",
-}: {
-    packagePath: string;
-    network: Network;
-    exportFileName: string;
-}) => {
-    const txb = new Transaction();
-
-    const { modules, dependencies } = JSON.parse(
-        execSync(`${SUI_BIN} move build --dump-bytecode-as-base64 --path ${packagePath}`, {
-            encoding: "utf-8",
-        }),
-    );
-
-    const cap = txb.publish({
-        modules,
-        dependencies,
-    });
-
-    // Transfer the upgrade capability to the sender so they can upgrade the package later if they want.
-    txb.transferObjects([cap], getActiveAddress());
-
-    const results = await signAndExecute(txb, network);
-
-    // @ts-ignore-next-line
-    const packageId = results.objectChanges?.find((x) => x.type === "published")?.packageId;
-
-    // save to an env file
-    writeFileSync(
-        `${exportFileName}.json`,
-        JSON.stringify({
-            packageId,
-        }),
-        { encoding: "utf8", flag: "w" },
-    );
 };
